49f235ab16387d231385ec0dd3c2e37996e4dfb4
Yves Senn <yves.senn@gmail.com>
2016-02-24 11:26:25 +0100

    house a dummy rails application for testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'railstash'
 
+require File.expand_path("../../test/dummy/config/environment.rb",  __FILE__)
 require 'minitest/autorun'
